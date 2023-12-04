a738d9d1db8c84a73847ed7f04aea1131126e5a8
handler <handler2104@gmail.com>
2023-12-04 18:42:49 +0100

    include

--- include/ft_ls.h
+++ include/ft_ls.h
@@ -1,6 +1,17 @@
 #ifndef FT_LS_H
 #define FT_LS_H
 
-# include <unistd.h>
+# include <unistd.h> //write
+# include <sys/types.h> //opendir, closedir
+# include <sys/stat.h> //stat, lstat
+# include <dirent.h> //readdir
+# include <pwd.h> // pwd
+# include <grp.h> //getgrgid
+# include <sys/xattr.h> //listxattr, getxattr
+# include <time.h> //time, ctime
+# include <fcntl.h> //readlink
+# include <stdlib.h> //malloc, free, exit
+# include <errno.h> //perror
+# include <string.h> //strerror
 
 #endif
